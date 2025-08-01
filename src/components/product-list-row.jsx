782a799bbb619824748f8d6f3f9af74e4a098a50
Matthew Janczak <mjhawk28@yahoo.com>
2025-08-01 01:39:56 -0500

    Dev - Created product list row component Then added it to the artwork page and started creating sections of artwork.

---
+++ src/components/product-list-row.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "./card";
+
+import artworks from "../data/artworks.json"; // Assuming artworks data is in this file
+
+const ProductListRow = ({ catergoryTitle, catergoryLink }) => {
+    return (
+        <div>
+            <div className="flex justify-between items-center py-4">
+                {catergoryTitle && (<h2 class="text-2xl font-bold tracking-tight text-white">{catergoryTitle}</h2>)}
+                {catergoryLink && (<a href={catergoryLink} className="text-blue-400 hover:underline">View All</a>)}
+            </div>
+            <div className="product-list-row grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+                {artworks.slice(0, 4).map((product) => (
+
+
+                    <Card
+                        key={product.id}
+                        title={product.title}
+                        image={product.image}
+                        description={product.description}
+                        price={product.price}
+                        saleprice={product.salePrice} />
+
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default ProductListRow;
--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -1,12 +1,24 @@
 // src/pages/AboutPage.js
 import React from 'react';
-
+import ProductListRow from '../components/product-list-row';
 const ArtworkPages = () => {
   return (
     <div>
       <div className='max-w-[1440px] flex flex-col justify-center items-center h-1/4 mx-auto'>
         <h1>Artwork Pages</h1>
         <p>This is the about page content.</p>
+
+        <ProductListRow 
+          catergoryTitle='Featured Artworks'
+          catergoryLink='#Link'/>
+
+          <ProductListRow 
+          catergoryTitle='New Arrivals'
+          catergoryLink='#Link'/>
+
+        <ProductListRow  
+        catergoryTitle='Dead By Daylight'
+        catergoryLink='#Link'/>
       </div>
 
     </div>
