f1ac408e64b9a1fe6835a8b23e1a204ef2b56962
Matthew Janczak <mjhawk28@yahoo.com>
2025-08-01 16:36:51 -0500

    Tweak - Add about page and contact page. Also update the routes.jsx to allow going through about, contact and pdp

--- src/header.jsx
+++ src/header.jsx
@@ -11,6 +11,8 @@ function Header() {
                 <nav className="main-nav flex justify-center align-center -ml-20 space-x-4">
                     <Link to="/" className="hover:underline">Home</Link>
                     <Link to="/artwork" className="hover:underline">Artwork/Collections</Link>
+                    <Link to="/about" className="hover:underline">About</Link>
+                    <Link to="/contact" className="hover:underline">Contact</Link>
                 </nav>
 
                 <nav className="space-x-4 flex items-center align-center">
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './home';
 import ArtworkPages from './pages/Artwork';
+import PDP from './pages/PDP';
 
 function AppRoutes() {
   return (
@@ -11,6 +12,10 @@ function AppRoutes() {
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="artwork" element={<ArtworkPages />} />
+          <Route path="about" element={<div>About Page</div>} />
+          <Route path="contact" element={<div>Contact Page</div>} />  
+          <Route path="pdp" element={<PDP />} />          
+          <Route path="*" element={<div>Page Not Found</div>} />
         </Route>
       </Routes>
     </Router>
