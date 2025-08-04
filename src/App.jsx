beabf6bc263e919c350e0a31cdce2ede513e7c83
Matthew Janczak <mjhawk28@yahoo.com>
2025-08-04 18:26:49 -0500

    Dev - Add a scroll to top when chaning pages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Header from './header';
 import Footer from './footer'; // Assuming you have a Footer component
 import { Outlet } from 'react-router-dom';
 
+import ScrollToTop from './components/scroll-to-top'; // Import the ScrollToTop component
+
 function App() {
   return (
     <div>
       <Header />
+      <ScrollToTop /> {/* Add the ScrollToTop component here */}
       {/* Other components and content go here */}
       <main className="bg-gray-950 text-white min-h-screen pt-[130px] ">
         <Outlet />
