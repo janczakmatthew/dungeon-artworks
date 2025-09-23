6b0ec3ce618a10488a34ae470eb67224568ace5f
Matthew Janczak <mjhawk28@yahoo.com>
2025-09-23 00:01:13 -0500

    Update - Add icons to the sidebar

--- src/admin/components/sidebar.jsx
+++ src/admin/components/sidebar.jsx
@@ -1,36 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { LuHouse, LuTag, LuGrid2X2, LuTags, LuShoppingCart, LuUser, LuSettings } from 'react-icons/lu';
+
 
 export default function Sidebar() {
   return (
-    <div className="w-64 bg-gray-800 text-white min-h-screen p-4">
+    <div className="w-64 bg-gray-800 text-md text-white min-h-screen">
         {/* <h2 className="text-2xl font-bold mb-6">Admin Panel</h2> */}
         <nav>
             <ul>
                 <li className="mb-4">
-                    <Link to="/admin" className="hover:underline">Dashboard</Link>
+                    <Link to="/admin" className="flex flex-row items-center w-full px-4 py-2 hover:bg-gray-700 transition">
+                    <LuHouse className="align-center mr-2" />
+                    Dashboard
+                    </Link>
                 </li>
                 <li className="mb-4">
-                    <Link to="/admin/products" className="hover:underline">Products</Link>  
+                    <Link to="/admin/products" className="flex flex-row items-center w-full px-4 py-2 hover:bg-gray-700 transition">
+                    <LuTag className="align-center mr-2" />
+                    Products
+                    </Link>  
                 </li>
                 <li className="mb-4">
-                    <Link to="/admin/categories" className="hover:underline">Categories</Link>
+                    <Link to="/admin/categories" className="flex flex-row items-center w-full px-4 py-2 hover:bg-gray-700 transition">
+                    <LuGrid2X2 className="align-center mr-2" />
+                    Categories
+                    </Link>
                 </li>
                 <li className="mb-4">
-                    <Link to="/admin/tags" className="hover:underline">Tags</Link>
+                    <Link to="/admin/tags" className="flex flex-row items-center w-full px-4 py-2 hover:bg-gray-700 transition">
+                    <LuTags className="align-center mr-2" />
+                    Tags
+                    </Link>
                 </li>
                 <li className="mb-4">
-                    <Link to="/admin/orders" className="hover:underline">Orders</Link>
+                    <Link to="/admin/orders" className="flex flex-row items-center w-full px-4 py-2 hover:bg-gray-700 transition">
+                    <LuShoppingCart className="align-center mr-2" />
+                    Orders
+                    </Link>
                 </li>
                 <li className="mb-4">           
-                    <Link to="/admin/users" className="hover:underline">Users</Link>
+                    <Link to="/admin/users" className="flex flex-row items-center w-full px-4 py-2 hover:bg-gray-700 transition">
+                    <LuUser className="align-center mr-2" />
+                    Users
+                    </Link>
                 </li>
                 {/* <li className="mb-4">
                     <Link to="/admin/reports" className="hover:underline">Reports</Link>
                 </li> */}
                 <li className="mb-4">
-                    <Link to="/admin/settings" className="hover:underline">Settings</Link>
+                    <Link to="/admin/settings" className="flex flex-row items-center w-full px-4 py-2 hover:bg-gray-700 transition">
+                    <LuSettings className="align-center mr-2" />
+                    Settings
+                    </Link>
                 </li>
             </ul>
         </nav>
